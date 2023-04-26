78c84020d76e0ba3f9688f9085e99a4fe96e1e6e
Alejandro Guerrero <59993294+guerreroag12@users.noreply.github.com>
2023-04-26 13:07:32 -0700

    Update Cipher.py
    modified code to allow for user input

--- Cipher.py
+++ Cipher.py
@@ -1,5 +1,5 @@
 plaintext_alphabet = "abcdefghijklmnopqrstuvwxyz" 
-ciphertext_alphabet = "ZGLEIYMAXDHROPKWTQNSCVUBJF" ##replace with given ciphertext
+ciphertext_alphabet = input("Enter ciphertext: ")
 
 encode_trans = str.maketrans(plaintext_alphabet, ciphertext_alphabet)
 decode_trans = str.maketrans(ciphertext_alphabet, plaintext_alphabet)
@@ -10,4 +10,4 @@ def subst_encode(s):
 def subst_decode(s):
     return s.translate(decode_trans)
 
-print(subst_decode("PXLI MKXPM DCPXKQ AZLHIQ"))  ##replace with given ciphertext to decode
+print(subst_decode(input("Enter ciphertext to decode: ")) 
